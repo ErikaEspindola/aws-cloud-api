27f46e69d851f8714ad37e55080d54a361444561
Erika <erikaaespindola@hotmail.com>
2019-05-01 17:13:49 -0300

    Criação de função parar criar instância spot

--- server.py
+++ server.py
@@ -3,7 +3,11 @@ from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
 from json import dumps
 from flask_jsonpify import jsonify
-import boto3, sys, configparser, socket
+import boto3
+import sys
+import configparser
+import socket
+import requests
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,28 +16,69 @@ secret_key = ''
 
 CORS(app)
 
+
 class Keys(Resource):
     def get(self):
         global access_key
         access_key = request.args.get('access_key')
         global secret_key
         secret_key = request.args.get('secret_key')
+
+        get_client()
+
         return {'status': 'ok'}
+
 
 api.add_resource(Keys, '/aws_keys')
 
+
+def get_client():
+    global client
+    client = boto3.client(
+        'ec2',
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        region_name='us-east-2'
+    )
+
+
 class Regions(Resource):
     def get(self):
-        client = boto3.client(
-            'ec2',
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key
-        )
-
         response = client.describe_regions()
         return response['Regions']
 
+
 api.add_resource(Regions, '/listar_regioes')
+
+
+class SpotInstance(Resource):
+    def get(self):
+        a = client.request_spot_instances(
+            InstanceCount = 1,
+            SpotPrice = "0.105",
+            LaunchSpecification =  {
+                "ImageId": "ami-0360d18a580e175cc",
+                "InstanceType": "t2.micro",
+                "BlockDeviceMappings": [
+                    {
+                        "DeviceName": "/dev/xvda",
+                        "Ebs": {
+                            "DeleteOnTermination": True,
+                            "VolumeType": "gp2",
+                            "VolumeSize": 8
+                        }
+                    }
+                ],
+            },
+            Type="one-time"
+        )
+
+        a["SpotInstanceRequests"][0]["CreateTime"] = str(a["SpotInstanceRequests"][0]["CreateTime"])
+        a["SpotInstanceRequests"][0]["Status"]["UpdateTime"] = str(a["SpotInstanceRequests"][0]["Status"]["UpdateTime"])
+
+        return a
+
+api.add_resource(SpotInstance, '/request_spot_instance')
 
 if __name__ == '__main__':
     app.run(port=5002, debug=True)
